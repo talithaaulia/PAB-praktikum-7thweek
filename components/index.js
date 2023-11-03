9917023fabb7f75a3d1714462e82b635ceed38f6
talithaaulia <talithaali88@gmail.com>
2023-11-03 23:30:03 +0700

    navdua

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import Header from "./header";
+import CategoryButton from "./category-button";
+import Categories from "./categories";
+import NewsItem from "./news-item";
+
+export { Header, CategoryButton, Categories, NewsItem };
+
